119cab4ced8a6f99646d288649cb34d3e7576d3b
locwa <94048557+locwa@users.noreply.github.com>
2025-01-29 11:42:31 +0800

    added add car button

--- resources/views/fleet.blade.php
+++ resources/views/fleet.blade.php
@@ -3,6 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Fleet') }}
         </h2>
+        <x-primary-button>
+            Add Car
+        </x-primary-button>
     </x-slot>
 
     <div class="py-12">
