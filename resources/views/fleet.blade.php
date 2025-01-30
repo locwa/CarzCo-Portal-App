43e699773ff00f368bedc0f5c61a477cc271379f
locwa <94048557+locwa@users.noreply.github.com>
2025-01-30 19:37:37 +0800

    changed window height

--- resources/views/fleet.blade.php
+++ resources/views/fleet.blade.php
@@ -11,7 +11,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex gap-4">
             @foreach($fleet_list as $list)
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-80 h-64">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-80 h-80">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <p class="font-light text-xl">{{ $list->make }}</p>
                         <h1 class="font-bold text-4xl">{{ $list->model }}</h1>
