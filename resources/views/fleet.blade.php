95e355d58185aeac2f003b7fd454a9b24aaa0d88
locwa <94048557+locwa@users.noreply.github.com>
2025-02-02 00:05:55 +0800

    restyled fleet windows

--- resources/views/fleet.blade.php
+++ resources/views/fleet.blade.php
@@ -9,9 +9,9 @@
     </x-slot>
 
     <div class="py-12 px-2">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex gap-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-wrap gap-4">
             @foreach($fleet_list as $list)
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-80 h-80">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-72 h-96">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <p class="font-light text-xl">{{ $list->make }}</p>
                         <h1 class="font-bold text-4xl">{{ $list->model }}</h1>
