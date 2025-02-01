4719335127a15cd0e398e1a2b384ab074376b79c
locwa <94048557+locwa@users.noreply.github.com>
2025-02-02 00:10:56 +0800

    used asset helper in favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('favicon.svg') }}" type="image/x-icon">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
