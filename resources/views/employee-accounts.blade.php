c6f1dd22d65baa3d0d627df2cbcb1045e5faf010
locwa <94048557+locwa@users.noreply.github.com>
2025-02-02 12:11:41 +0800

    replaced all buttons with just view profile button

--- resources/views/employee-accounts.blade.php
+++ resources/views/employee-accounts.blade.php
@@ -14,19 +14,17 @@
         @foreach($accounts as $employee)
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-3">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 flex items-center justify-between">
+
                 <div class="text-gray-900 dark:text-gray-100">
                     <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight mb-4">{{ $employee->name }}</h2>
                     <p class="font-light text-sm text-gray-800 dark:text-gray-200 leading-tight mb-1">{{ $employee->email }}</p>
                     <p class="font-light text-sm text-gray-800 dark:text-gray-200 leading-tight">{{ $employee->is_admin ? "Admin" : "User" }}</p>
                 </div>
-                <div>
-                    <a href="./edit-profile/{{ $employee->id }}">
-                        <x-danger-button>Delete</x-danger-button>
-                    </a>
-                    <a href="./edit-profile/{{ $employee->id }}">
-                        <x-primary-button>{{ $employee->is_admin ? "Demote To User" : "Make Admin" }}</x-primary-button>
-                    </a>
-                </div>
+
+                <a href="./view-profile/{{ $employee->id }}">
+                    <x-primary-button>View Profile</x-primary-button>
+                </a>
+
             </div>
         </div>
         @endforeach
