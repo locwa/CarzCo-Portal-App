dd1c1c9f2ef9623a1c9ca56a02aada4cefef458f
locwa <94048557+locwa@users.noreply.github.com>
2025-02-02 11:08:01 +0800

    added feature of adding employee accounts

--- resources/views/employee-accounts.blade.php
+++ resources/views/employee-accounts.blade.php
@@ -3,21 +3,30 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Employee Accounts') }}
         </h2>
-        <x-primary-button>
-            Add Employee Account
-        </x-primary-button>
+        <a href="./register">
+            <x-primary-button>
+                Add Employee Account
+            </x-primary-button>
+        </a>
     </x-slot>
 
     <div class="py-12 px-2">
         @foreach($accounts as $employee)
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-3">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 flex items-center justify-between">
                 <div class="text-gray-900 dark:text-gray-100">
                     <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight mb-4">{{ $employee->name }}</h2>
                     <p class="font-light text-sm text-gray-800 dark:text-gray-200 leading-tight mb-1">{{ $employee->email }}</p>
                     <p class="font-light text-sm text-gray-800 dark:text-gray-200 leading-tight">{{ $employee->is_admin ? "Admin" : "User" }}</p>
                 </div>
-                <x-primary-button class="h-12">Edit</x-primary-button>
+                <div>
+                    <a href="./edit-profile/{{ $employee->id }}">
+                        <x-danger-button>Delete</x-danger-button>
+                    </a>
+                    <a href="./edit-profile/{{ $employee->id }}">
+                        <x-primary-button>{{ $employee->is_admin ? "Demote To User" : "Make Admin" }}</x-primary-button>
+                    </a>
+                </div>
             </div>
         </div>
         @endforeach
