664d4e8a96bcc7e4675511c8f4f2470d91702a39
locwa <94048557+locwa@users.noreply.github.com>
2025-02-03 12:34:43 +0800

    changed link

--- resources/views/edit-car.blade.php
+++ resources/views/edit-car.blade.php
@@ -3,8 +3,8 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Edit Car') }}
         </h2>
-        <a href="../fleet">
-            <x-primary-button>Back to Fleet</x-primary-button>
+        <a href="../view-car/{{ $car_details->value('id') }}">
+            <x-primary-button>Back to Car</x-primary-button>
         </a>
     </x-slot>
 
