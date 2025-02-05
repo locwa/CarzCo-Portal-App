bfcf563edda7d1d1820fec38e7da70b66d2caaf8
locwa <94048557+locwa@users.noreply.github.com>
2025-02-05 20:50:25 +0800

    added enctype to form tag

--- resources/views/edit-car.blade.php
+++ resources/views/edit-car.blade.php
@@ -11,7 +11,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            <form method="post" action="../edit-car/{{ $car_details->value('id') }}" class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <form method="post" action="../edit-car/{{ $car_details->value('id') }}" class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" enctype="multipart/form-data">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @csrf
 
